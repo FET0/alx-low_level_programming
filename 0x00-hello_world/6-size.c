7a63b4b1c2d29b3c712f7f51118064d35301f34a
FET0 <fuhadtadese@gmail.com>
2022-03-11 02:13:26 -0800

    the sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ float e;
 printf("Size of a char: %u bytes(s)\n", sizeof(a));
 printf("Size of an int: %u bytes(s)\n", sizeof(b));
 printf("Size of a long int: %u bytes(s)\n", sizeof(c));
-printf("Size of a long long int: lu byte(s)\n", sizeof(d));
+printf("Size of a long long int: %u byte(s)\n", sizeof(d));
 printf("Size of a float: %u byte(s)\n", sizeof(e));
 
 return (0);
