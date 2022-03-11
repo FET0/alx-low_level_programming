c5f097ebcddfb8cd402ba269f8cbc71ad4f0a28c
FET0 <fuhadtadese@gmail.com>
2022-03-11 01:17:48 -0800

    the sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int b;
 long int c;
 long long int d;
 float e;
-printf("size of char; %d bytes\n", sizeof (a));
-printf("size of  int; %d bytes\n", sizeof (b));
-printf("size of  long int; %d bytes\n", sizeof (c));
-printf("size of long long int; %d bytes\n", sizeof (d));
-printf("size of float; %d bytes\n", sizeof (e));
+printf("size of char; %d bytes(s)\n", sizeof (a));
+printf("size of  int; %d bytes(s)\n", sizeof (b));
+printf("size of  long int; %d bytes(s)\n", sizeof (c));
+printf("size of long long int; %d byte(s)\n", sizeof (d));
+printf("size of float; %d byte(s)\n", sizeof (e));
 return (0);
 }
