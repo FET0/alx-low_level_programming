bec783bc1d7fa75465da1ac0d7a00d6dc14d0873
FET0 <fuhadtadese@gmail.com>
2022-03-15 17:36:25 -0700

    nested_loop header n1

--- 0x02-functions_nested_loops/alxheader.h
+++ 0x02-functions_nested_loops/alxheader.h
@@ -12,7 +12,7 @@ int _print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void pr_digit(int charcode, int fmt);
-int add(int a, int b,);
+int add(int a, int b);
 void pr_with_fmt(int charcode, int fmt);
 void print_to_98(int n);
 void pr_3_digit(int charcode, int fmt);
