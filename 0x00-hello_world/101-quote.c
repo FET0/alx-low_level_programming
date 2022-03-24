71c5e6de8471132874c11ea2b3d1a77d672c773f
FET0 <fuhadtadese@gmail.com>
2022-03-24 07:07:16 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * main - prints to the standard error
+ *
+ * Return : (1) Success
+ */
+int main(){
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,11 @@
 #include <unistd.h>
 
 /*
- *  *_puts - prints a string, to stdout
+ *  _puts - prints a string, to stdout
  *
- *   *@str: value to be evaluate.
+ *   @str: value to be evaluate.
  *
- *    *Return: not.
- *
+ *    Return: not.
  */
 
 void _puts(char *str)
