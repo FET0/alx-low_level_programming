f0286df3d883b6b7e4a0e552c03637777d012d16
FET0 <fuhadtadese@gmail.com>
2022-04-07 07:16:43 -0700

    corrections

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: print alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int x;
+for(x = 97; x >= 122; x++)
+{
+putchar(x);
+}
+
+return (0);
+}
