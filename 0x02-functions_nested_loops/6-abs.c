94b4bfd2ec2ec0f88fa36969da9aff030e31fce2
FET0 <fuhadtadese@gmail.com>
2022-04-22 18:06:07 -0700

    correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry Point
  *
- * description: print absolute value of int
+ * description for _abs: print absolute value of int
  *
  * Return:Always (0) success
  */
@@ -10,10 +10,12 @@ int _abs(int n)
 {
 if (n < 0)
 {
-return(n * -1);
-} else if (n > 0)
+return (n * -1);
+}
+else if (n > 0)
 {
-return(n);
-} else
+return (n);
+}
+else
 return (0);
 }
